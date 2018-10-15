5046b53a62b3a80d19672844025af842cccfc853
irfan maulanasam <alternatiflab@gmail.com>
2018-10-15 21:36:08 +0700

    indentasi fixing

--- exercise14sorting.js
+++ exercise14sorting.js
@@ -1,29 +1,29 @@
 function sorting(arrNumber) {
-  return arrNumber.sort (function(a,b) {
-    return a - b;
-});
+  return arrNumber.sort (function(a,b) {return a - b;});
 }
   
-  function getTotal(arrNumber) {
-    var frequent = 0;
-    var largest = arrNumber [arrNumber.length - 1 ];
-    if (arrNumber == 0) {
+function getTotal( arrNumber ) {
+  var frequent = 0;
+  var largest = arrNumber [ arrNumber.length - 1 ];
+    if ( arrNumber == 0 ) {
       return "''"; // UNTUK TEST CASE YANG TERAKHIR
-    } else {
-      for (var num of arrNumber) {
-        if (num === arrNumber [arrNumber.length - 1]) {
+      }
+      else
+      {
+      for ( var num of arrNumber ) {
+        if ( num === arrNumber [arrNumber.length - 1] ) {
           frequent = frequent + 1;
         }
       }
     }
-    return 'angka paling besar adalah ' + largest + ' dan jumlah kemunculan sebanyak ' + frequent + ' kali';
+  return 'angka paling besar adalah ' + largest + ' dan jumlah kemunculan sebanyak ' + frequent + ' kali';
 }
   
-  function mostFrequentLargestNumbers(arrNumber) {
-    var listSort = sorting(arrNumber);
-    var countHighest = getTotal(listSort);
-    return countHighest;
-  }
+function mostFrequentLargestNumbers( arrNumber ) {
+  var listSort = sorting( arrNumber );
+  var countHighest = getTotal( listSort );
+  return countHighest;
+}
   
   console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
   //'angka paling besar adalah 8 dan jumlah kemunculan sebanyak 3 kali'
