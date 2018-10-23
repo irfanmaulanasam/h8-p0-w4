148d5c35ad005b92878b56ec540e3d06d118511a
irfan maulanasam <alternatiflab@gmail.com>
2018-10-23 21:10:38 +0700

    menampung kata ke object

--- exercise6.js
+++ exercise6.js
@@ -1,47 +1,45 @@
 function hitungHuruf(kata) {
-  var pisahKata = kata.split(' ');//memisahkan tiap kata untuk dijadikan array
-  var perulanganKata = []; //menampung perulangan pada setiap kata
-  var ulangHuruf = [];
-  for(var i = 0; i < pisahKata.length; i++){
-    var pisahHuruf = pisahKata[i].split(''); //membuat array dari 1 kata
-    var perulanganHuruf = 0; //penampung jumlah pengulangan huruf di setiap kata
-    for(var j = 0; j < pisahKata[i].length -1; j++){
-      for(var k = j; k < pisahHuruf.length; k++){
-        
-        if(j !== k && pisahKata[i][j] === pisahHuruf[k]){
-          perulanganHuruf ++;//penghitungan jumlah pengulangan huruf pada setiap kata
-          var splice = pisahHuruf.splice(k, 1);
-          
-        }
-        
-      }
-      //console.log(perulanganHuruf)
-    }
-    perulanganKata.push(perulanganHuruf);
-  } 
-  console.log(pisahKata)
-  console.log(perulanganKata)
-  for( var a=0 ; a<pisahKata.length ; a++){
-    for( var b=0 ; b<perulanganKata ; b++){
-      if( perulanganKata>){
-        
-      }
+  let arr = kata.split(' ')
+  let object = {};
+  
+  for(let i = 0; i < arr.length; i++) {
+    if(object[arr[i]] === undefined) {
+      object[arr[i]] = 0
     }
   }
+  for(let j = 0; j < arr.length; j++) {
+    let newObject = {}
+      for(let k = 0; k < arr[j].length; k++) {
+        if(newObject[arr[j][k]] === undefined) {
+          newObject[arr[j][k]] = 1
+        } else {
+          newObject[arr[j][k]] += 1
+        }
+      }
 
-  // var hurufTerbanyak = Math.max.apply(null, perulanganKata); //menentukan score pengulangan paling besar
-  // var lokasiPerulanganTerbesar = perulanganKata.indexOf(hurufTerbanyak);//Cari lokasi array dari array kata yang udah di split
-  // // console.log(lokasiPerulanganTerbesar)
-  // if(lokasiPerulanganTerbesar !== 0){
-  //   return pisahKata[lokasiPerulanganTerbesar];
-  // }else{
-  //   return -1;
-  // }
+      let scoreKata = 0;
+
+      for (let key in newObject) {
+        if (newObject[key] > 1) {
+          scoreKata += newObject[key]
+        }
+      }
+      object[arr[j]] = scoreKata
+    }
+  let word
+  let scoreWord = 0
+
+  for (let x in object) {
+    if(object[x] > scoreWord) {
+      word = x
+      scoreWord = object[x]
+    }
+  }
+  return word
 }
-
 // TEST CASES
 console.log(hitungHuruf('Today, is the greatest day ever')); // greatest
-// console.log(hitungHuruf('I am a passionate developer')); // passionate
-// console.log(hitungHuruf('aku adalah anak gembala')); // adalah
-// console.log(hitungHuruf('rajin pangkal kaya')); // pangkal
-// console.log(hitungHuruf('mengayuh perahu di danau')); // danau
+console.log(hitungHuruf('I am a passionate developer')); // passionate
+console.log(hitungHuruf('aku adalah anak gembala')); // adalah
+console.log(hitungHuruf('rajin pangkal kaya')); // pangkal
+console.log(hitungHuruf('mengayuh perahu di danau')); // danau
